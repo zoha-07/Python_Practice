17c368bbd337a5c4e1ce104af338c712cd41a0b5
zoha_07 <the.real.zoha@gmail.com>
2026-07-10 13:45:54 +0500

    strings practice added

---
+++ Strings_practice.py
@@ -0,0 +1,9 @@
+#input users first name and print its lenght
+
+#Name=input("Enter your first name:")
+#lenght=len(Name)
+#print("the lenght of your name is:",lenght)
+
+#find occurence of D 
+Variable="today is a sunny day "
+print("The occurence of the letter d is:",Variable.count("d"))
