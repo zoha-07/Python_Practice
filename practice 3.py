5942f0fc7a199df1f0cd01eb1c762e981a217e11
zoha_07 <the.real.zoha@gmail.com>
2026-07-10 13:56:59 +0500

    removed practice 3 file

--- practice 3.py
+++
@@ -1,9 +0,0 @@
-# if the number is even or odd 
-Number=int(input("Enter the Number:"))
-
-if(Number % 2==0):
-    print("The Number you entered is even")
-else:
-    print("The Number you entered is odd")
-# find the greatest number 
-# multiple of 7 or not 
