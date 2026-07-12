3b7be63bffd0c5eb0157d88d309a5acf1a9cc3c5
zoha_07 <the.real.zoha@gmail.com>
2026-07-12 12:13:09 +0500

    added conditional statements practice

---
+++ Conditional Statements.py
@@ -0,0 +1,27 @@
+#odd or even Number 
+#num=int(input("Enter your Number:"))
+#if(num%2==0):
+   # print("The number you entered is even")
+#else:
+   # print("The number you entered is odd")
+
+#find the greatest number
+
+#Number1=int(input("Enter your first number:"))
+#Number2=int(input("Enter your second number:"))
+#Number3=int(input("Enter your third number:"))
+
+#if(Number1>=Number2 and Number1>=Number3):
+      #print(Number1,"is the greater number")
+#elif(Number2>=Number3):
+      #print(Number2,("is the greater number"))
+#else:
+      #print(Number3,"is the greater number")
+
+#Multiple of 7 or not 
+x=int(input("Enter your number:"))
+if(x%7==0):
+    print("The number is a multiple of 7")
+else:
+    print("The number is not a multiple of 7")
+
